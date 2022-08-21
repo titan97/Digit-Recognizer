7c00d07c1be2d22295511cf3b0d744dac7b9dfab
Thao Ngo <22862806+titan97@users.noreply.github.com>
2022-08-20 23:19:00 -0700

    update

--- app.py
+++ app.py
@@ -1,15 +1,43 @@
-from urllib import response
-from flask import Flask, render_template, request
+import json
+
+import torch
+from torchvision import models
+import torchvision.transforms as transforms
+from flask import Flask, jsonify, request
+
 
 app = Flask(__name__)
+model = torch.load('model.pth')
+model.eval()
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+def transform_image(image_bytes):
+    my_transforms = transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    image = Image.open(io.BytesIO(image_bytes))
+    return my_transforms(image).unsqueeze(0)
 
 
-@app.route('/predict/', methods=['GET', 'POST'])
+def get_prediction(image_bytes):
+    tensor = transform_image(image_bytes=image_bytes)
+    outputs = model.forward(tensor)
+    _, y_hat = outputs.max(1)
+    predicted_idx = str(y_hat.item())
+    return imagenet_class_index[predicted_idx]
+
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    response = "For ML Prediction"
-    return response
+    if request.method == 'POST':
+        file = request.files['file']
+        img_bytes = file.read()
+        class_id, class_name = get_prediction(image_bytes=img_bytes)
+        return jsonify({'class_id': class_id, 'class_name': class_name})
+
+
+if __name__ == '__main__':
+    app.run()
